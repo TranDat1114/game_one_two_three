bca32a3eb9c6bed49f7db8b03574b88d0b00014f
TranDat1114 <88397960+TranDat1114@users.noreply.github.com>
2021-09-05 23:50:30 +0700

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
+# Import thư viện random
 import random
+# Import hàm nhà làm\ * tức là import tất cả
 from for_game import *
 
-#Variables
+# Variables
 downtext = ' '
 text_won = 'Bạn Thắng Rồi!!!'
 text_lose = 'Bạn Thua Rồi!!!'
@@ -12,18 +14,23 @@ draw_point = 0
 match = 0
 running = True
 
-#Try to input values
+# Gọi là game thì phải chơi được liên tục và while true: là cách để thực hiện việc đó
 while running:
-	#Variables
+	# Variables
 	number = 7
 	player = 0
 	draw_test = False
 	won_test = True
 	test_input = True
 	
+	# Try to input values
+	# Thuật toán là nếu nhập vào khác số từ 1 đến 15 thì buộc nhập lại
 	while test_input:
+		# Gọi hàm làm sạch terminal
 		screen_clear()
+		# Hiện ra các dữ liệu trò chơi gồm: dict[menu chọn từ 1 - 15]; điểm
 		showing_data(win_point, draw_point, lose_point, match)
+		# Try - except tức là bắt lỗi mục đích để handle nếu có lỗi do người dùng tạo ra
 		try:
 			player = int(input('Nhập vào một số tương ứng: '))
 		except ValueError:
@@ -32,16 +39,20 @@ while running:
 		if (player > 0) and (player < 16):
 			test_input = False
 
-
+	# Cách tạo 1 list
 	list_a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+	# Random trong list a
 	com = random.choice(list_a)
-
+	
+	# Đơn giản là một cách print giá trị và chuỗi hơi khác bình thường một tý
 	print(f'Bạn chọn {dictData[player]}')
 	print(f'Máy chọn {dictData[com]}')
-
+	
+	# Thuật toán logic của trò chơi này
 	# nếu player + 7 > 15: nếu player - 7 < com and player > com: thua
 	# nếu player - 7 < 0 : nếu player + 7 > com and player < com: thắng
-
+	# Có nhiều cách và mình dùng cái này vì nó đơn giãn nhất
+	
 	if player == com:
 			draw_test = True
 	elif player + 7 > 15:
@@ -55,9 +66,16 @@ while running:
 	else:
 		if player > com:
 			won_test = False
-			
+	
+	# Xuống dòng cho đẹp thôi bình thường thì là '\n'
+	# Nhưng powerShell của mình cứ sau mỗi lần print là con trỏ chuột tự xuống dòng
+	# Nên mình dùng '__' (__: ký hiệu cho khoảng trống : Space)
 	print(downtext)
-
+	
+	# Show_point
+	# Dùng để tăng điểm sau mỗi vòng chơi
+	# Bạn có thể tách hàm show_data thành thánh 1 hàm mới tên là show_point và gọi nó sau đoạn if - else này 
+	# Để nó cập nhật điểm ngay lập tức
 	if draw_test == True:
 		print(text_draw)
 		draw_point = draw_point + 1
@@ -69,16 +87,22 @@ while running:
 		lose_point = lose_point + 1
 
 	print(downtext)
-	match = match + 1
-	test = True
 	
+	# Tương tự show_point nhưng là đếm số trận đã chơi
+	match = match + 1
+	
+	# Test is variables for testing input
+	test = True
 	print('chơi tiếp hay không?: ')
 	while test:
+		# if users have wrong input program show input again
 		play_again = str(input('Yes(y)/ No(n) \n'))
-		# WTF I'M I DOING
+		
+		#func_input() là hàm trong for_game.py
 		if func_input(play_again) == True:
 			test = False	
-		#PLEASE STOP DOING THAT HOÀNG 
+		# PLEASE STOP DOING THAT HOÀNG
+		# Why u have too much commit bruh? :L
 		elif func_input(play_again) == False:
 			test = False
 			running = False
